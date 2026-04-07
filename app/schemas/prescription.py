643940da4abdfc5382f36a2645aaa846bbb126e8
toki513 <tokitahmidkhan@gmail.com>
2026-04-08 03:14:28 +0600

    updated

---
+++ app/schemas/prescription.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from uuid import UUID
+from datetime import datetime
+from typing import Optional
+
+
+class PrescriptionCreate(BaseModel):
+    patient_name:str
+    medications:str
+    
+    
+class PrescriptionResponse(BaseModel):
+    id:UUID
+    patient_name:str
+    medication:str
+    created_at:datetime
+    doctor_id:UUID
+    
+    model_config={"from_attributes":True}
